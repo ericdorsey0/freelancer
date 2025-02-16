0bae1d5b37be86ebf9b4824dfa44010db29fb1c4
eric <dorseyjr000@gmail.com>
2025-02-16 01:52:13 -0600

    .\index.css

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+const Freelancer = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
+const averageRate = getavrageRate();
+
+function makeFreelancer() {
+  const name = sample(NAMES);
+  const occupation = sample(OCCUPATIONS);
+  const rate =
+    PRICE_RANGE.min +
+    Math.floor(Mayh.random() * (PRICE_RANGE - PRICE_RANGE, min));
+
+  return { name, occupation, rate };
+}
+
+function getAvrageRate() {}
